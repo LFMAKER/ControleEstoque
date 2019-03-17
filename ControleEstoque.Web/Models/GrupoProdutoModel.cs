82b08ef676c624e9483ad1a5405b4c0c6ecdb65e
lfmak <lfmaker@outlook.com>
2019-03-17 19:56:59 -0300

    (CRUD GrupoProduto) Validation Implement

--- ControleEstoque.Web/Models/GrupoProdutoModel.cs
+++ ControleEstoque.Web/Models/GrupoProdutoModel.cs
@@ -8,8 +8,11 @@ namespace ControleEstoque.Web.Models
 {
     public class GrupoProdutoModel
     {
+
         public int Id { get; set; }
+        [Required(ErrorMessage ="Preencha o nome.")]
         public string Nome { get; set; }
+     
         public bool Ativo { get; set; }
 
     }
