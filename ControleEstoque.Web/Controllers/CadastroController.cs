80d90213a96f6603be0558a09465cea559008d05
lfmak <lfmaker@outlook.com>
2019-04-06 08:05:44 -0300

    CRUD Usuario

--- ControleEstoque.Web/Controllers/CadastroController.cs
+++ ControleEstoque.Web/Controllers/CadastroController.cs
@@ -9,7 +9,131 @@ namespace ControleEstoque.Web.Controllers
 {
     public class CadastroController : Controller
     {
-       
+
+        #region Usuários
+        private const string _senhaPadrao = "{$127;$188}";
+
+        [Authorize]
+        public ActionResult Usuario()
+        {
+
+
+            ViewBag.SenhaPadrao = _senhaPadrao;
+            return View(UsuarioModel.RecuperarLista());
+        }
+
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult RecuperarUsuario(int id)
+        {
+            return Json(UsuarioModel.RecuperarPeloId(id));
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult ExcluirUsuario(int id)
+        {
+            return Json(UsuarioModel.ExcluirPeloId(id));
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult SalvarUsuario(UsuarioModel model)
+        {
+            var resultado = "OK";
+            var mensagens = new List<string>();
+            var idSalvo = string.Empty;
+
+            if (!ModelState.IsValid)
+            {
+                resultado = "AVISO";
+                mensagens = ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).ToList();
+            }
+            else
+            {
+                try
+                {
+
+                    if(model.Senha == _senhaPadrao)
+                    {
+                        model.Senha = "";
+                    }
+
+
+                    var id = model.Salvar();
+                    if (id > 0)
+                    {
+                        idSalvo = id.ToString();
+                    }else
+                    {
+                        resultado = "ERRO";
+                    }
+                   
+
+                }
+                catch (Exception ex)
+                {
+                    resultado = "ERRO";
+                }
+            }
+
+            return Json(new { Resultado = resultado, Mensagens = mensagens, IdSalvo = idSalvo });
+        }
+
+        #endregion
+
+        [Authorize]
+        public ActionResult MarcaProduto()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult LocalProduto()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult UnidadeMedida()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Produto()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Pais()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Estado()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Cidade()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Fornecedor()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult PerfilUsuario()
+        {
+            return View();
+        }
+
+
+        #region Grupos de Produtos
 
 
         [Authorize]
@@ -57,11 +181,12 @@ namespace ControleEstoque.Web.Controllers
                     if (id > 0)
                     {
                         idSalvo = id.ToString();
-                    }else
+                    }
+                    else
                     {
                         resultado = "ERRO";
                     }
-                   
+
 
                 }
                 catch (Exception ex)
@@ -73,56 +198,6 @@ namespace ControleEstoque.Web.Controllers
             return Json(new { Resultado = resultado, Mensagens = mensagens, IdSalvo = idSalvo });
         }
 
-
-        [Authorize]
-        public ActionResult MarcaProduto()
-        {
-            return View();
-        }
-        [Authorize]
-        public ActionResult LocalProduto()
-        {
-            return View();
-        }
-        [Authorize]
-        public ActionResult UnidadeMedida()
-        {
-            return View();
-        }
-        [Authorize]
-        public ActionResult Produto()
-        {
-            return View();
-        }
-        [Authorize]
-        public ActionResult Pais()
-        {
-            return View();
-        }
-        [Authorize]
-        public ActionResult Estado()
-        {
-            return View();
-        }
-        [Authorize]
-        public ActionResult Cidade()
-        {
-            return View();
-        }
-        [Authorize]
-        public ActionResult Fornecedor()
-        {
-            return View();
-        }
-        [Authorize]
-        public ActionResult PerfilUsuario()
-        {
-            return View();
-        }
-        [Authorize]
-        public ActionResult Usuario()
-        {
-            return View();
-        }
+        #endregion
     }
 }
