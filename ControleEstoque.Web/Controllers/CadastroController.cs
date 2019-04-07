248d9056c2155810fb8a14521f4723be4098cd79
lfmak <lfmaker@outlook.com>
2019-04-07 03:03:11 -0300

    Add Pagination

--- ControleEstoque.Web/Controllers/CadastroController.cs
+++ ControleEstoque.Web/Controllers/CadastroController.cs
@@ -9,6 +9,7 @@ namespace ControleEstoque.Web.Controllers
 {
     public class CadastroController : Controller
     {
+        private const int _quantMaxLinhasPorPagina = 5;
 
         #region Usuários
         private const string _senhaPadrao = "{$127;$188}";
@@ -86,6 +87,7 @@ namespace ControleEstoque.Web.Controllers
 
         #endregion
 
+       
         [Authorize]
         public ActionResult MarcaProduto()
         {
@@ -133,71 +135,5 @@ namespace ControleEstoque.Web.Controllers
         }
 
 
-        #region Grupos de Produtos
-
-
-        [Authorize]
-        public ActionResult GrupoProduto()
-        {
-            return View(GrupoProdutoModel.RecuperarLista());
-        }
-
-
-        [HttpPost]
-        [Authorize]
-        [ValidateAntiForgeryToken]
-        public ActionResult RecuperarGrupoProduto(int id)
-        {
-            return Json(GrupoProdutoModel.RecuperarPeloId(id));
-        }
-
-        [HttpPost]
-        [Authorize]
-        [ValidateAntiForgeryToken]
-        public ActionResult ExcluirGrupoProduto(int id)
-        {
-            return Json(GrupoProdutoModel.ExcluirPeloId(id));
-        }
-
-        [HttpPost]
-        [Authorize]
-        [ValidateAntiForgeryToken]
-        public ActionResult SalvarGrupoProduto(GrupoProdutoModel model)
-        {
-            var resultado = "OK";
-            var mensagens = new List<string>();
-            var idSalvo = string.Empty;
-
-            if (!ModelState.IsValid)
-            {
-                resultado = "AVISO";
-                mensagens = ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).ToList();
-            }
-            else
-            {
-                try
-                {
-                    var id = model.Salvar();
-                    if (id > 0)
-                    {
-                        idSalvo = id.ToString();
-                    }
-                    else
-                    {
-                        resultado = "ERRO";
-                    }
-
-
-                }
-                catch (Exception ex)
-                {
-                    resultado = "ERRO";
-                }
-            }
-
-            return Json(new { Resultado = resultado, Mensagens = mensagens, IdSalvo = idSalvo });
-        }
-
-        #endregion
     }
 }
