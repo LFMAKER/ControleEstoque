854e02084a25a85a2ee625b72ebbb5152fc0c3a2
lfmak <lfmaker@outlook.com>
2019-06-03 01:31:59 -0300

    Add CPJ and CNPJ validation in Fornecedor

--- ControleEstoque.Web/Scripts/Cadastro/Fornecedor.js
+++ ControleEstoque.Web/Scripts/Cadastro/Fornecedor.js
@@ -24,7 +24,7 @@
         $('#cbx_pessoa_fisica').prop('checked', true).trigger('click');
     }
 
-  
+
 
 }
 
@@ -104,7 +104,41 @@ $(document)
     $('label[for="txt_num_documento"]').text('CPF');
     $('#txt_num_documento').mask('000.000.000-00', { reverse: true });
     $('#container_razao_social').addClass('invisible');
+}).on('keyup', '#txt_num_documento', function () {
+
+    if ($('#cbx_pessoa_fisica').is(':checked')) {
+
+        var valido = validarCPF($(this).val())
+        if (valido) {
+            $(this).removeClass('is-invalid');
+            $(this).addClass('is-valid');
+        } else {
+            $(this).removeClass('is-valid');
+            $(this).addClass('is-invalid');
+        }
+    } else {
+        var valido = validarCNPJ($(this).val())
+        if (valido) {
+            $(this).removeClass('is-invalid');
+            $(this).addClass('is-valid');
+        } else {
+            $(this).removeClass('is-valid');
+            $(this).addClass('is-invalid');
+        }
+    }
+
+
+
+}).on('change', '#cbx_pessoa_juridica', function () {
+    $('#txt_num_documento').removeClass('is-invalid');
+    $('#txt_num_documento').removeClass('is-valid');
+    $('#txt_num_documento').val('');
+}).on('change', '#cbx_pessoa_fisica', function () {
+    $('#txt_num_documento').removeClass('is-invalid');
+    $('#txt_num_documento').removeClass('is-valid');
+    $('#txt_num_documento').val('');
 });
+
 
 
 $('#txt_cep').on('blur', function () {
@@ -119,15 +153,105 @@ $('#txt_cep').on('blur', function () {
             $("#txt_cidade").val(dados.cidade);
             $("#txt_estado").val(dados.estado_info.nome);
             $("#txt_pais").val("Brasil");
-           
+
         } //end if.
         else {
             //CEP pesquisado não foi encontrado.
-           
+
             alert("CEP não encontrado.");
         }
     });
 });
 
+function validarCPF(cpf) {
+    cpf = cpf.replace(/[^\d]+/g, '');
+    if (cpf == '') return false;
+    // Elimina CPFs invalidos conhecidos	
+    if (cpf.length != 11 ||
+		cpf == "00000000000" ||
+		cpf == "11111111111" ||
+		cpf == "22222222222" ||
+		cpf == "33333333333" ||
+		cpf == "44444444444" ||
+		cpf == "55555555555" ||
+		cpf == "66666666666" ||
+		cpf == "77777777777" ||
+		cpf == "88888888888" ||
+		cpf == "99999999999" ||
+		cpf == "12345678909")
+        return false;
+    // Valida 1o digito	
+    add = 0;
+    for (i = 0; i < 9; i++)
+        add += parseInt(cpf.charAt(i)) * (10 - i);
+    rev = 11 - (add % 11);
+    if (rev == 10 || rev == 11)
+        rev = 0;
+    if (rev != parseInt(cpf.charAt(9)))
+        return false;
+    // Valida 2o digito	
+    add = 0;
+    for (i = 0; i < 10; i++)
+        add += parseInt(cpf.charAt(i)) * (11 - i);
+    rev = 11 - (add % 11);
+    if (rev == 10 || rev == 11)
+        rev = 0;
+    if (rev != parseInt(cpf.charAt(10)))
+        return false;
+    return true;
+}
 
 
+function validarCNPJ(cnpj) {
+
+    cnpj = cnpj.replace(/[^\d]+/g, '');
+
+    if (cnpj == '') return false;
+
+    if (cnpj.length != 14)
+        return false;
+
+    // Elimina CNPJs invalidos conhecidos
+    if (cnpj == "00000000000000" ||
+        cnpj == "11111111111111" ||
+        cnpj == "22222222222222" ||
+        cnpj == "33333333333333" ||
+        cnpj == "44444444444444" ||
+        cnpj == "55555555555555" ||
+        cnpj == "66666666666666" ||
+        cnpj == "77777777777777" ||
+        cnpj == "88888888888888" ||
+        cnpj == "99999999999999")
+        return false;
+
+    // Valida DVs
+    tamanho = cnpj.length - 2
+    numeros = cnpj.substring(0, tamanho);
+    digitos = cnpj.substring(tamanho);
+    soma = 0;
+    pos = tamanho - 7;
+    for (i = tamanho; i >= 1; i--) {
+        soma += numeros.charAt(tamanho - i) * pos--;
+        if (pos < 2)
+            pos = 9;
+    }
+    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+    if (resultado != digitos.charAt(0))
+        return false;
+
+    tamanho = tamanho + 1;
+    numeros = cnpj.substring(0, tamanho);
+    soma = 0;
+    pos = tamanho - 7;
+    for (i = tamanho; i >= 1; i--) {
+        soma += numeros.charAt(tamanho - i) * pos--;
+        if (pos < 2)
+            pos = 9;
+    }
+    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+    if (resultado != digitos.charAt(1))
+        return false;
+
+    return true;
+
+}
