43e7233572039521cb14792fddb64ab00dececab
lfmak <lfmaker@outlook.com>
2019-06-04 17:38:54 -0300

    Export PDF AND XLSX in Relatorios

--- ControleEstoque.Web/Controllers/Relatorio/RelatorioController.cs
+++ ControleEstoque.Web/Controllers/Relatorio/RelatorioController.cs
@@ -7,19 +7,124 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using NPOI;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System.IO;
+using NPOI.HSSF.UserModel;
+using NPOI.HSSF.Util;
+using ControleEstoque.Web.Dal.Relatorio;
 
 namespace ControleEstoque.Web.Controllers.Relatorio
 {
     public class RelatorioController : Controller
     {
-        // GET: Relatorio
-        /*
-         @todo: Export PDF "Relatorio"
-         @body: Create a controller to return a report in pdf
-             */
-
-
+        
+        //RELATORIO ENTRADAS
         public ActionResult RelatorioEntradasIndex()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RelatorioEntradasFiltro(DateTime txt_data_inicio, DateTime txt_data_fim, int cbx_tipo)
+        {
+            //cbx_tipo = 1, significa gerar PDF
+            //cbx_tipo = 2, significa gerar XLSX
+
+            if (cbx_tipo == 1)
+            {
+                //Criando o PDF
+                var relatorioPDF = new ViewAsPdf
+                {
+                    ViewName = "RelatorioEntradasPDF",
+                    IsGrayScale = false,
+                    FileName = "RelatorioEntradasPDF.pdf",
+                    Model = EntradaESaidaRelatorioDao.GetEntradasRelatorioFiltro(txt_data_inicio, txt_data_fim)
+                };
+                return relatorioPDF;
+            }
+            else
+            {
+                List<EntradaGraficos> entradas = EntradaESaidaRelatorioDao.GetEntradasRelatorioFiltro(txt_data_inicio, txt_data_fim);
+                //Criando o XLSX
+                IWorkbook workbook = new XSSFWorkbook();
+                ISheet sheet = workbook.CreateSheet("Relatório Entradas");
+                int rowNumer = 0;
+
+                //---- HEADER
+                IRow row = sheet.CreateRow(rowNumer);
+                ICell cellData;
+                ICell cellTotal;
+                ICellStyle styleData = workbook.CreateCellStyle();
+                styleData.FillForegroundColor = NPOI.HSSF.Util.HSSFColor.Violet.Index;
+                styleData.Alignment = HorizontalAlignment.Center;
+
+                ICellStyle styleTotal = workbook.CreateCellStyle();
+                styleTotal.Alignment = HorizontalAlignment.Right;
+                ICreationHelper createHelper = workbook.GetCreationHelper();
+                styleTotal.DataFormat = (createHelper.CreateDataFormat().GetFormat("R$ #.##0;-R$ #.##0"));
+
+
+                cellData = row.CreateCell(0);
+                cellData.SetCellValue("Data");
+                cellData.CellStyle = styleData;
+
+                cellTotal = row.CreateCell(1);
+                cellTotal.SetCellValue("Total");
+                cellTotal.CellStyle = styleTotal;
+                if (entradas.Count > 0 ){
+                    foreach (var item in entradas)
+                    {
+                        rowNumer++;
+                        row = sheet.CreateRow(rowNumer);
+                        cellData = row.CreateCell(0);
+                        cellData.SetCellValue(item.Data.ToString("dd/MM/yyyy"));
+                        cellData.CellStyle = styleData;
+
+                        cellTotal = row.CreateCell(1);
+                        cellTotal.SetCellValue((double)item.total);
+                        cellTotal.CellStyle = styleTotal;
+                    }
+                }else
+                {
+                    rowNumer++;
+                    row = sheet.CreateRow(rowNumer);
+                    cellData = row.CreateCell(0);
+                    cellData.SetCellValue("SEM DADOS NESSE PERIODO");
+                    cellData.CellStyle = styleData;
+
+                    cellTotal = row.CreateCell(1);
+                    cellTotal.SetCellValue(0.00);
+                    cellTotal.CellStyle = styleTotal;
+                }
+                //Tamanho das colunas
+                sheet.SetColumnWidth(0, 40 * 256);
+                sheet.SetColumnWidth(1, 20 * 256);
+
+
+
+                MemoryStream stream = new MemoryStream();
+                workbook.Write(stream);
+                return File(stream.ToArray(), //The binary data of the XLS file
+        "application/vnd.ms-excel", //MIME type of Excel files
+        string.Format("RelatorioEntradas_{0}.xlsx", DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"))); //Suggested file name in the "Save as" dialog which will be displayed to the end user
+            }
+        }
+
+
+        public async Task<ActionResult> RelatorioEntradas()
+        {
+            List<EntradaGraficos> entradas = await EntradaESaidaGraficoDao.GetEntradasGrafico();
+            return View(entradas);
+        }
+
+
+
+
+        //RELATÓRIO SAIDA
+
+        public ActionResult RelatorioSaidasIndex()
         {
             return View();
         }
@@ -27,31 +132,99 @@ namespace ControleEstoque.Web.Controllers.Relatorio
 
         //GetEntradasGraficoFiltro
         [HttpPost]
-        public ActionResult RelatorioEntradasFiltro(DateTime txt_data_inicio, DateTime txt_data_fim, int cbx_tipo)
+        public ActionResult RelatorioSaidasFiltro(DateTime txt_data_inicio, DateTime txt_data_fim, int cbx_tipo)
         {
+            //cbx_tipo = 1, significa gerar PDF
+            //cbx_tipo = 2, significa gerar XLSX
 
-            if(cbx_tipo == 1)
+            if (cbx_tipo == 1)
             {
+                //Criando o PDF
                 var relatorioPDF = new ViewAsPdf
                 {
-                    ViewName = "RelatorioEntradasPDF",
+                    ViewName = "RelatorioSaidasPDF",
                     IsGrayScale = false,
-                    FileName = "RelatorioEntradasPDF.pdf",
-                    Model = EntradaESaidaGraficoDao.GetEntradasGraficoFiltro(txt_data_inicio, txt_data_fim)
+                    FileName = "RelatorioSaidasPDF.pdf",
+                    Model = EntradaESaidaRelatorioDao.GetSaidasRelatorioFiltro(txt_data_inicio, txt_data_fim)
                 };
                 return relatorioPDF;
             }
-            return null;
+            else
+            {
+                List<SaidaGraficos> saidas = EntradaESaidaRelatorioDao.GetSaidasRelatorioFiltro(txt_data_inicio, txt_data_fim);
+                //Criando o XLSX
+                IWorkbook workbook = new XSSFWorkbook();
+                ISheet sheet = workbook.CreateSheet("Relatório Entradas");
+                int rowNumer = 0;
+
+                //---- HEADER
+                IRow row = sheet.CreateRow(rowNumer);
+                ICell cellData;
+                ICell cellTotal;
+                ICellStyle styleData = workbook.CreateCellStyle();
+                styleData.FillForegroundColor = NPOI.HSSF.Util.HSSFColor.Violet.Index;
+                styleData.Alignment = HorizontalAlignment.Center;
+
+                ICellStyle styleTotal = workbook.CreateCellStyle();
+                styleTotal.Alignment = HorizontalAlignment.Right;
+                ICreationHelper createHelper = workbook.GetCreationHelper();
+                styleTotal.DataFormat = (createHelper.CreateDataFormat().GetFormat("R$ #.##0;-R$ #.##0"));
+
+
+                cellData = row.CreateCell(0);
+                cellData.SetCellValue("Data");
+                cellData.CellStyle = styleData;
+
+                cellTotal = row.CreateCell(1);
+                cellTotal.SetCellValue("Total");
+                cellTotal.CellStyle = styleTotal;
+                if (saidas.Count > 0)
+                {
+                    foreach (var item in saidas)
+                    {
+                        rowNumer++;
+                        row = sheet.CreateRow(rowNumer);
+                        cellData = row.CreateCell(0);
+                        cellData.SetCellValue(item.Data.ToString("dd/MM/yyyy"));
+                        cellData.CellStyle = styleData;
+
+                        cellTotal = row.CreateCell(1);
+                        cellTotal.SetCellValue((double)item.total);
+                        cellTotal.CellStyle = styleTotal;
+                    }
+                }
+                else
+                {
+                    rowNumer++;
+                    row = sheet.CreateRow(rowNumer);
+                    cellData = row.CreateCell(0);
+                    cellData.SetCellValue("SEM DADOS NESSE PERIODO");
+                    cellData.CellStyle = styleData;
+
+                    cellTotal = row.CreateCell(1);
+                    cellTotal.SetCellValue(0.00);
+                    cellTotal.CellStyle = styleTotal;
+                }
+                //Tamanho das colunas
+                sheet.SetColumnWidth(0, 40 * 256);
+                sheet.SetColumnWidth(1, 20 * 256);
+
+
+
+                MemoryStream stream = new MemoryStream();
+                workbook.Write(stream);
+                return File(stream.ToArray(), //The binary data of the XLS file
+        "application/vnd.ms-excel", //MIME type of Excel files
+        string.Format("RelatorioSaidas_{0}.xlsx", DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"))); //Suggested file name in the "Save as" dialog which will be displayed to the end user
+            }
         }
 
 
-        public async Task<ActionResult> RelatorioEntradas()
+        public async Task<ActionResult> RelatorioSaidas()
         {
-
-            List<EntradaGraficos> entradas = await EntradaESaidaGraficoDao.GetEntradasGrafico();
-            return View(entradas);
+            List<SaidaGraficos> saidas = await EntradaESaidaGraficoDao.GetSaidasGrafico();
+            return View(saidas);
         }
-
 
 
 
---
+++ ControleEstoque.Web/Dal/Relatorio/EntradaESaidaRelatorioDao.cs
@@ -0,0 +1,65 @@
+﻿
+using ControleEstoque.Web.Models;
+using ControleEstoque.Web.Models.Domain;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ControleEstoque.Web.Dal.Relatorio
+{
+    public class EntradaESaidaRelatorioDao
+    {
+
+        public static List<EntradaGraficos> GetEntradasRelatorioFiltro(DateTime dataInicio, DateTime dataFim)
+        {
+            List<EntradaGraficos> entradas = new List<EntradaGraficos>();
+
+
+            using (var ctx = new Context())
+            {
+
+                //entradas = ctx.EntradasProdutos.Include("Produto").ToList();
+                var sql = "SELECT FORMAT (data, 'MM/yyyy') as data, SUM((preco_custo*quant)) as total " +
+                "from entrada_produto " +
+                 "inner join produto on produto.id = entrada_produto.id_produto " +
+                 "WHERE(YEAR(data) BETWEEN " + dataInicio.Year + " and " + dataFim.Year + ") and" +
+                 "(MONTH(data) BETWEEN " + dataInicio.Month + " and " + dataFim.Month + ")" +
+                  "GROUP BY FORMAT(data, 'MM/yyyy')";
+
+                entradas = ctx.Database.Connection.Query<EntradaGraficos>(sql).ToList();
+                entradas = entradas.OrderBy(x => x.Data).ToList();
+
+            }
+            return entradas;
+        }
+
+
+        public static List<SaidaGraficos> GetSaidasRelatorioFiltro(DateTime dataInicio, DateTime dataFim)
+        {
+            List<SaidaGraficos> saidas = new List<SaidaGraficos>();
+
+
+            using (var ctx = new Context())
+            {
+
+                //entradas = ctx.EntradasProdutos.Include("Produto").ToList();
+                var sql = "SELECT FORMAT (data, 'MM/yyyy') as data, SUM((preco_custo*quant)) as total " +
+                "from saida_produto " +
+                 "inner join produto on produto.id = saida_produto.id_produto " +
+                 "WHERE(YEAR(data) BETWEEN " + dataInicio.Year + " and " + dataFim.Year + ") and" +
+                 "(MONTH(data) BETWEEN " + dataInicio.Month + " and " + dataFim.Month + ")" +
+                  "GROUP BY FORMAT(data, 'MM/yyyy')";
+
+                saidas = ctx.Database.Connection.Query<SaidaGraficos>(sql).ToList();
+                saidas = saidas.OrderBy(x => x.Data).ToList();
+
+            }
+            return saidas;
+        }
+
+
+
+    }
+}
